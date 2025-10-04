9fcb24175f510c767a2390d61a685c3e580884e3
blue <blue@silentium.io>
2025-10-04 20:54:41 +0700

    correct script

--- src/specify_cli/__init__.py
+++ src/specify_cli/__init__.py
@@ -435,7 +435,7 @@ def init_git_repo(project_path: Path, quiet: bool = False) -> bool:
 
 
 def download_template_from_github(ai_assistant: str, download_dir: Path, *, script_type: str = "sh", verbose: bool = True, show_progress: bool = True, client: httpx.Client = None, debug: bool = False, github_token: str = None) -> Tuple[Path, dict]:
-    repo_owner = "github"
+    repo_owner = "thnh-dng"
     repo_name = "spec-kit"
     if client is None:
         client = httpx.Client(verify=ssl_context)
